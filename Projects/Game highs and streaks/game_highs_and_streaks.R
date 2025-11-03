90a27c68154c93daf11112b98b7bf07343e246be
smalinowski7 <seth.w.malinowski@gmail.com>
2025-11-02 20:29:19 -0500

    Finished single-game/streak media plots and script

--- Projects/Game highs and streaks/game_highs_and_streaks.R
+++ Projects/Game highs and streaks/game_highs_and_streaks.R
@@ -89,7 +89,8 @@ goalie_boxscores_rg <- goalie_boxscores %>%
 ### Pick a stat here and calculate the top single game leaders
 ### Stat should be how it appears on the boxscores
 
-stat <- "BS"
+stat <- "TK"
+plot_filter <- 7
 
 sg_stat_leaders <- boxscores_rg %>%
   arrange(desc(get(stat))) %>%
@@ -98,7 +99,7 @@ sg_stat_leaders <- boxscores_rg %>%
 
 sg_plot <- sg_stat_leaders %>%
   
-  filter(get(stat) >= 13) %>%
+  filter(get(stat) >= plot_filter) %>%
   
   mutate(unique_id = paste0(name, Game.Id)) %>%
   mutate(label = paste0(name, " (S", season, ")")) %>%
@@ -113,7 +114,10 @@ ggplot(sg_plot, aes(x = .data[[stat]], y = fct_rev(unique_id))) +
   scale_fill_manual(values = team_colors) +
   theme_bw(base_size = 14) +
   theme(panel.grid = element_blank()) +
-  labs(title = paste0("Single game ", stat, " high"), y = NULL, x = paste0(stat, "s"))
+  labs(title = paste0("Single game ", stat, " high"), y = NULL, x ="Takeaways")
+
+ggsave(here("Projects/Game highs and streaks/plots/single_game_tk.png"),height = 9, width = 7, dpi = 300)
+
 
 ##########################
 ### Single game multis ###
@@ -122,7 +126,7 @@ ggplot(sg_plot, aes(x = .data[[stat]], y = fct_rev(unique_id))) +
 ### Pick a stat here and calculate the number of mulit-stat games
 ### Stat should be how it appears on the boxscores
 
-stat <- "HT"
+stat <- "TK"
 
 mg_stat_leaders <- boxscores_rg %>%
   mutate(multi_stat = ifelse(get(stat) > 1, 1, 0)) %>%
@@ -165,7 +169,7 @@ ggplot(mg_stat_plot, aes(x = `get(stat)`, y = n, fill = abbreviation)) +
             size = 6) +
   ylim(c(0, max(mg_stat_plot$n)*1.25))
 
-ggsave("test.jpg", height = 15, width = 7, dpi = 300)
+ggsave(here("Projects/Game highs and streaks/plots/multi_tk.png"), height = 15, width = 7, dpi = 300)
 
 
 
@@ -177,7 +181,7 @@ ggsave("test.jpg", height = 15, width = 7, dpi = 300)
 ### Pick a stat here and calculate the streak number for players
 ### Stat should be how it appears on the boxscores
 
-stat <- "HT"
+stat <- "BS"
 
 streaks <- boxscores_rg %>%
   mutate(streak_stat = ifelse(get(stat) > 0, 1, 0)) %>%
@@ -197,7 +201,8 @@ stat_streaks <- streaks %>%
     
   ) %>%
   ungroup() %>%
-  arrange(desc(streak_length))
+  arrange(desc(streak_length)) %>%
+  mutate(rank = row_number()) 
 
 
 streak_plot <- stat_streaks %>%
@@ -207,7 +212,7 @@ streak_plot <- stat_streaks %>%
                            TRUE ~ paste0(name, " (S", season_start, "-S", season_end, ")"))) %>%
   mutate(plot_id = paste0(label, steak_id)) %>%
   mutate(plot_id = factor(plot_id, levels = unique(plot_id))) %>%
-  filter(streak_length >= 30)
+  filter(rank <= 20)
 
 
 
@@ -219,10 +224,116 @@ ggplot(streak_plot, aes(x = streak_length, y = fct_rev(plot_id), fill = streak_l
   theme(panel.grid = element_blank()) +
   scale_y_discrete(breaks = streak_plot$plot_id, 
                    labels = streak_plot$label) +
-  scale_fill_gradient(low = "grey85", high = "#5C0029") +
-  labs(x = "Streak length", y = NULL, title = "SHL hit streaks")
+  scale_fill_gradient(low = "grey85", high = "#52485B") +
+  labs(x = "Streak length", y = NULL, title = "SHL shot block streaks")
 
-ggsave("Projects/Game highs and streaks/plots/hit_streak.jpg", 
+ggsave("Projects/Game highs and streaks/plots/streak_bs.png", 
        width = 6.5,
        height = 7.5,
        dpi = 300)
+
+
+
+################################
+### Goalie single game highs ###
+################################
+
+### Pick a stat here and calculate the top single game leaders
+### Stat should be how it appears on the boxscores
+
+stat <- "GSAA"
+
+goalie_sg_stat_leaders <- goalie_boxscores_rg %>%
+  arrange(desc(get(stat))) %>%
+  select(season, abbreviation, name, all_of(stat), everything())
+
+
+goalie_sg_plot <- goalie_sg_stat_leaders %>%
+  
+ mutate(rank = row_number()) %>%
+  filter(rank <= 20) %>%
+  
+  mutate(unique_id = paste0(name, Game.Id)) %>%
+  mutate(label = paste0(name, " (S", season, ")")) %>%
+  mutate(unique_id = factor(unique_id, levels = unique_id))
+
+ggplot(goalie_sg_plot, aes(x = .data[[stat]], y = fct_rev(unique_id))) +
+  geom_col(aes(fill = abbreviation),
+           col = "black",
+           show.legend = F) +
+  scale_y_discrete(breaks = goalie_sg_plot$unique_id, 
+                   labels = goalie_sg_plot$label) +
+  scale_fill_manual(values = team_colors) +
+  theme_bw(base_size = 14) +
+  theme(panel.grid = element_blank()) +
+  labs(title = paste0("Single game ", stat, " high"), y = NULL, x = "GSAA")
+
+ggsave(here("Projects/Game highs and streaks/plots/goalie_single_game_gsaa.png"),
+       width = 6.5,
+       height = 7.5,
+       dpi = 300)
+
+
+
+
+
+
+###########################
+### Goalie Stat streaks ###
+###########################
+
+### Pick a stat here and calculate the streak number for players
+### Stat should be how it appears on the boxscores
+
+stat <- "SV"
+
+goalie_streaks <- goalie_boxscores_rg %>%
+  mutate(streak_stat = ifelse(get(stat) > 29, 1, 0)) %>%
+  group_by(PlayerId) %>%
+  mutate(steak_id = cumsum(c(1, diff(streak_stat) != 0))) %>% 
+  ungroup()
+
+goalie_stat_streaks <- goalie_streaks %>%
+  filter(streak_stat == 1) %>%
+  group_by(PlayerId, steak_id) %>%
+  summarise(
+    season_start = min(season),
+    season_end = max(season),
+    team = paste0(unique(abbreviation), collapse = ","),
+    name = Mode(name),
+    streak_length = n()
+    
+  ) %>%
+  ungroup() %>%
+  arrange(desc(streak_length)) %>%
+  mutate(rank = row_number()) 
+
+
+goalie_streak_plot <- goalie_stat_streaks %>%
+  ungroup() %>%
+  arrange(desc(streak_length)) %>%
+  mutate(label = case_when(season_start == season_end ~ paste0(name, " (S", season_start, ")"),
+                           TRUE ~ paste0(name, " (S", season_start, "-S", season_end, ")"))) %>%
+  mutate(plot_id = paste0(label, steak_id)) %>%
+  mutate(plot_id = factor(plot_id, levels = unique(plot_id))) %>%
+  filter(rank <= 20)
+
+
+
+ggplot(goalie_streak_plot, aes(x = streak_length, y = fct_rev(plot_id), fill = streak_length)) +
+  geom_col(col = "black",
+           show.legend = F,
+           width = .85) +
+  theme_bw(base_size = 14) +
+  theme(panel.grid = element_blank()) +
+  scale_y_discrete(breaks = goalie_streak_plot$plot_id, 
+                   labels = goalie_streak_plot$label) +
+  scale_fill_gradient(low = "grey85", high = "red3") +
+  labs(x = "Streak length", y = NULL, title = "SHL 30+ save streaks")
+
+ggsave("Projects/Game highs and streaks/plots/goalie_streak_sv.png", 
+       width = 6.5,
+       height = 7.5,
+       dpi = 300)
+
+
