f98a43e50f8cfdadbea65cbe5f1ddaec4440fab6
smalinowski7 <seth.w.malinowski@gmail.com>
2026-01-29 11:01:12 -0500

    Finished history catchup code writing

--- Projects/History catchup and code/history_catchup.R
+++ Projects/History catchup and code/history_catchup.R
@@ -22,7 +22,10 @@ team_abb_map <- team_abb_map %>%
   select(id, abbreviation)
 
 
-team_meta_j <- read.csv("Data/SMJHL/index_team_meta.csv") %>%
+team_meta_j <- read.csv("Data/SMJHL/index_team_meta.csv") 
+
+
+team_abb_map_j <- team_meta_j %>%
   mutate(abbreviation = ifelse(abbreviation == "ANA", "YUM", abbreviation)) %>%
   mutate(abbreviation = ifelse(abbreviation == "REG", "TBW", abbreviation)) %>%
   group_by(abbreviation) %>%
@@ -80,6 +83,93 @@ j_first_round <- draft_df %>%
 shl_standings <- read_csv("Data/SHL/index_standings.csv")
 
 j_standings <- read_csv("Data/SMJHL/index_standings.csv")
+
+
+# Load and format playoff schedules
+po_sch <- read_csv("Data/SHL/Playoffs/index_schedule.csv") %>%
+  filter(type == "Playoffs")
+
+po_list <- list()
+for (i in unique(team_abb_map$id)) {
+  print(i)
+  temp_df <- po_sch %>%
+    filter(homeTeam == i | awayTeam == i) %>%
+    mutate(team = case_when(homeTeam == i ~ homeTeam,
+                            awayTeam == i ~ awayTeam),
+           opp = case_when(homeTeam == i ~ awayTeam,
+                           awayTeam == i ~ homeTeam),
+           score = case_when(homeTeam == i ~ homeScore,
+                             awayTeam == i ~ awayScore),
+           opp_score = case_when(homeTeam == i ~ awayScore,
+                                 awayTeam == i~ homeScore),
+           win = 1*(score > opp_score))
+  po_list[[i+1]] <- temp_df
+}
+po_sch_formatted <- do.call(rbind, po_list) 
+po_sch_sum <- po_sch_formatted %>%
+  arrange(date) %>%
+  group_by(season, team) %>%
+  mutate(round = match(opp, unique(opp))) %>%
+  group_by(season, team) %>%
+  filter(round == max(round)) %>%
+  group_by(season, team, round) %>%
+  summarise(opp = opp[1],
+            games_won = sum(win),
+            games_lost = length(win) - games_won) %>%
+  ungroup() %>%
+  mutate(outcome = ifelse(games_won > games_lost, "Won", "Lost in"),
+         round_name = case_when(round == 1 ~ "Round 1",
+                                round == 2 ~ "Round 2",
+                                round == 3 ~ "Conference Finals",
+                                round == 4 ~ "Challenge Cup"),
+         losing_games = ifelse(games_won == 4, games_lost, games_won)) %>%
+  left_join(select(team_meta, id, name, season), by = c("opp" = "id", "season")) %>%
+  rename("opp_abb" = "name") %>%
+  mutate(label = paste0(outcome, " ", round_name, " against ", opp_abb, " (4-", losing_games, ")"))
+
+
+# Repeat for the J
+po_sch_j <- read_csv("Data/SMJHL/Playoffs/index_schedule.csv") %>%
+  filter(type == "Playoffs")
+
+po_list_j <- list()
+for (i in unique(team_abb_map_j$id)) {
+  print(i)
+  temp_df <- po_sch_j %>%
+    filter(homeTeam == i | awayTeam == i) %>%
+    mutate(team = case_when(homeTeam == i ~ homeTeam,
+                            awayTeam == i ~ awayTeam),
+           opp = case_when(homeTeam == i ~ awayTeam,
+                           awayTeam == i ~ homeTeam),
+           score = case_when(homeTeam == i ~ homeScore,
+                             awayTeam == i ~ awayScore),
+           opp_score = case_when(homeTeam == i ~ awayScore,
+                                 awayTeam == i~ homeScore),
+           win = 1*(score > opp_score))
+  po_list_j[[i+1]] <- temp_df
+}
+po_sch_formatted_j <- do.call(rbind, po_list_j) 
+po_sch_sum_j <- po_sch_formatted_j %>%
+  arrange(date) %>%
+  group_by(season, team) %>%
+  mutate(round = match(opp, unique(opp))) %>%
+  group_by(season, team) %>%
+  filter(round == max(round)) %>%
+  group_by(season, team, round) %>%
+  summarise(opp = opp[1],
+            games_won = sum(win),
+            games_lost = length(win) - games_won) %>%
+  ungroup() %>%
+  mutate(outcome = ifelse(games_won > games_lost, "Won", "Lost in"),
+         round_name = case_when(round == 1 ~ "Round 1",
+                                round == 2 ~ "Round 2",
+                                round == 3 ~ "Conference Finals",
+                                round == 4 ~ "Four Star Cup"),
+         losing_games = ifelse(games_won == 4, games_lost, games_won)) %>%
+  left_join(select(team_meta_j, id, name, season), by = c("opp" = "id", "season")) %>%
+  rename("opp_abb" = "name") %>%
+  mutate(label = paste0(outcome, " ", round_name, " against ", opp_abb, " (4-", losing_games, ")"))
+
 
 #################################
 ### Championship team summary ###
@@ -234,7 +324,9 @@ hof_by_team <- rs_stats %>%
 shl_profile_summary <- function(team_id, 
                                 season_record_last_updated, 
                                 min_draft_season, 
-                                current_draft_season) {
+                                current_draft_season,
+                                min_playoff_season,
+                                max_playoff_season) {
   
   team_abbr <- team_abb_map$abbreviation[team_abb_map$id == team_id]
   
@@ -242,6 +334,8 @@ shl_profile_summary <- function(team_id,
     stop("Team abbreviation is not length of 1")
   }
   
+  # Format team record
+  # Season by season and also total record since the last recorded season
   team_record <- shl_standings %>%
     left_join(select(team_meta, id, season, abbreviation, conference),
               by = c("id", "season", "abbreviation")) %>%
@@ -262,7 +356,7 @@ shl_profile_summary <- function(team_id,
     filter(id == team_id, season > season_record_last_updated) %>%
     mutate(conference = ifelse(conference == 1, "Western", "Eastern")) %>%
     arrange(season) %>%
-    mutate(label = paste0("S", season, ": ", wins, "-", losses, "-", OTL, " (", conf_suffix, " in ", conference, ", ", lg_suffix, " in Leeague)"))
+    mutate(label = paste0("S", season, ": ", wins, "-", losses, "-", OTL, " (", conf_suffix, " in ", conference, ", ", lg_suffix, " in League)"))
     
   team_record_combined <- team_record %>%
     ungroup() %>%
@@ -270,13 +364,25 @@ shl_profile_summary <- function(team_id,
   
   
   
+  # Format playoff results
+  playoff_results <- po_sch_sum %>%
+    filter(team == team_id)
   
+  playoffs_null <- data.frame(season = min_playoff_season:max_playoff_season) %>%
+    left_join(select(playoff_results, season, label)) %>%
+    mutate(label = ifelse(is.na(label), "Did not qualify", label)) %>%
+    mutate(final_label = paste0("S", season, ": ", label))
+  
+  # Collect team HoF players
+  # Will have to do manually for HoF GMs later
   team_hof <- hof_by_team %>%
     filter(team == team_abbr) %>%
     left_join(select(hof_df, playerID, designation, seasonInducted), by = c("id" = "playerID")) %>%
     mutate(label = paste0("S", seasonInducted, " - ", designation, " - ", name, " (", seasons, ")"))
   
   
+  
+  # Format and merge all team awards
   team_awards <- team_awards_df %>%
     filter(leagueID == 0, teamID == team_id) %>%
     rename("season" = "seasonID",
@@ -319,6 +425,8 @@ shl_profile_summary <- function(team_id,
     select(-season)
   
   
+  
+  # Format team first round picks
   first_rounders <- shl_first_round %>%
     filter(teamID == team_id) %>%
     arrange(seasonID) %>%
@@ -338,19 +446,149 @@ shl_profile_summary <- function(team_id,
     mutate(label = paste0("S", seasonID, ": ", int_label))
   
   
+  
+  
   write_lines(combined_awards$label, paste0("Projects/History catchup and code/", team_abbr, "_awards.txt"))
   write_lines(first_round_null$label, paste0("Projects/History catchup and code/", team_abbr, "_draft.txt"))
   write_lines(team_record$label, paste0("Projects/History catchup and code/", team_abbr, "_rs_standings.txt"))
   write_lines(team_record_combined, paste0("Projects/History catchup and code/", team_abbr, "_rs_combined_record.txt"))
-  write_lines(team_hof, paste0("Projects/History catchup and code/", team_abbr, "_HoF.txt"))
+  write_lines(team_hof$label, paste0("Projects/History catchup and code/", team_abbr, "_HoF.txt"))
+  write_lines(playoffs_null$final_label, paste0("Projects/History catchup and code/", team_abbr, "_playoff_results.txt"))
   
   
   
 }
-results <- shl_profile_summary(team_id = 1, season_last_updated = 68)
-View(results[[1]])
-View(results[[2]])
-View(results[[3]])
-View(results[[4]])
-View(results[[5]])
-View(results[[6]])
+
+
+
+
+
+smjhl_profile_summary <- function(team_id, 
+                                season_record_last_updated, 
+                                min_draft_season, 
+                                current_draft_season,
+                                min_playoff_season,
+                                max_playoff_season) {
+  
+  team_abbr <- team_abb_map_j$abbreviation[team_abb_map_j$id == team_id]
+  
+  if (length(team_abbr) != 1) {
+    stop("Team abbreviation is not length of 1")
+  }
+  
+  # Format team record
+  # Season by season and also total record since the last recorded season
+  team_record <- j_standings %>%
+    left_join(select(team_meta_j, id, season, abbreviation, conference),
+              by = c("id", "season", "abbreviation")) %>%
+    group_by(season, conference) %>%
+    arrange(season, conference, desc(points), desc(ROW), desc(wins), desc(goalDiff)) %>%
+    mutate(conf_rank = row_number()) %>%
+    group_by(season) %>%
+    arrange(season, desc(points), desc(ROW), desc(wins), desc(goalDiff)) %>%
+    mutate(lg_rank = row_number()) %>%
+    mutate(conf_suffix = case_when(conf_rank == 1 ~ "1st",
+                                   conf_rank == 2 ~ "2nd",
+                                   conf_rank == 3 ~ "3rd",
+                                   TRUE ~ paste0(conf_rank, "th"))) %>%
+    mutate(lg_suffix = case_when(lg_rank == 1 ~ "1st",
+                                 lg_rank == 2 ~ "2nd",
+                                 lg_rank == 3 ~ "3rd",
+                                 TRUE ~ paste0(lg_rank, "th"))) %>%
+    filter(id == team_id, season > season_record_last_updated) %>%
+    mutate(conference = ifelse(conference == 1, "Southern", "Northern")) %>%
+    arrange(season) %>%
+    mutate(label = paste0("S", season, ": ", wins, "-", losses, "-", OTL, " (", conf_suffix, " in ", conference, ", ", lg_suffix, " in League)"))
+  
+  team_record_combined <- team_record %>%
+    ungroup() %>%
+    summarise(record = paste0(sum(wins), "-", sum(losses), "-", sum(OTL)))
+  
+  
+  
+  # Format playoff results
+  playoff_results <- po_sch_sum_j %>%
+    filter(team == team_id)
+  
+  playoffs_null <- data.frame(season = min_playoff_season:max_playoff_season) %>%
+    left_join(select(playoff_results, season, label)) %>%
+    mutate(label = ifelse(is.na(label), "Did not qualify", label)) %>%
+    mutate(final_label = paste0("S", season, ": ", label))
+  
+
+  
+  # Format and merge all team awards
+  team_awards <- team_awards_df %>%
+    filter(leagueID == 1, teamID == team_id) %>%
+    rename("season" = "seasonID",
+           "award" = "achievementName") %>%
+    mutate(award = factor(award, levels = c("Laurifer Trophy",
+                                            "King-Kurczewski",
+                                            "Linna-Landvik",
+                                            "Four Star Cup"))) %>%
+    arrange(season, award) %>%
+    mutate(label = paste0("S", season, ": ", award)) %>%
+    select(season, label)
+  
+  
+  player_awards <- player_award_winners %>%
+    filter(isAward == TRUE, leagueID == 1, teamID == team_id) %>%
+    rename("season" = "seasonID",
+           "award" = "achievementName") %>%
+    arrange(season) %>%
+    mutate(label = paste0("S", season, ": ", award, " - ", playerName))  %>%
+    select(season, label)
+  
+  
+  user_awards <- user_awards_df %>%
+    filter(leagueID == 1, 
+           won == TRUE, 
+           isAward == TRUE,
+           teamID == team_id) %>%
+    rename("season" = "seasonID",
+           "award" = "achievementName") %>%
+    arrange(season, award, GM) %>%
+    group_by(season, award) %>%
+    summarise(label = paste0(username, collapse = "/")) %>%
+    mutate(label = paste0("S", season, ": ", award, " - ", label)) %>%
+    ungroup() %>%
+    select(season, label)
+  
+  
+  combined_awards <- rbind(user_awards, player_awards, team_awards) %>%
+    arrange(season) %>%
+    select(-season)
+  
+  
+  
+  # Format team first round picks
+  first_rounders <- j_first_round %>%
+    filter(teamID == team_id) %>%
+    arrange(seasonID) %>%
+    mutate(int_label = ifelse(isExpansion == TRUE,
+                              paste0(playerName, " (", draftNumber, " OA)*"),
+                              paste0(playerName, " (", draftNumber, " OA)"))) %>%
+    group_by(seasonID) %>%
+    summarise(int_label = paste0(int_label, collapse = ", ")) 
+  
+  
+  first_round_null <- data.frame(seasonID = min_draft_season:current_draft_season) %>%
+    left_join(first_rounders) %>%
+    mutate(int_label = ifelse(is.na(int_label),
+                              "No pick",
+                              int_label)) %>%
+    arrange(seasonID) %>%
+    mutate(label = paste0("S", seasonID, ": ", int_label))
+  
+  
+  
+  
+  write_lines(combined_awards$label, paste0("Projects/History catchup and code/", team_abbr, "_awards.txt"))
+  write_lines(first_round_null$label, paste0("Projects/History catchup and code/", team_abbr, "_draft.txt"))
+  write_lines(team_record$label, paste0("Projects/History catchup and code/", team_abbr, "_rs_standings.txt"))
+  write_lines(team_record_combined, paste0("Projects/History catchup and code/", team_abbr, "_rs_combined_record.txt"))
+  write_lines(playoffs_null$final_label, paste0("Projects/History catchup and code/", team_abbr, "_playoff_results.txt"))
+  
+  
+  
+}
